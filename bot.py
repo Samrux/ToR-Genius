ee521c61ca07da428a3836ba8d4c3b899f9b7b9f
Samrux <samruxb@gmail.com>
2018-04-12 23:27:31 -0300

    neater

--- bot.py
+++ bot.py
@@ -247,7 +247,7 @@ class TorGenius(commands.Bot):
         if ctx.command is None:
             if "just monika" in message.content.lower():
                 await ctx.send('Just Monika')
-            elif message.content == 'neat':
+            elif message.content == 'neat' and message.user_id == 228656614851346432:
                 await ctx.send('neat')
             return
 
