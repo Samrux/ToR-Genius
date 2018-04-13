204fc8c1d43412264313f91dab3b2ad9d6f2cc8f
Samrux <samruxb@gmail.com>
2018-04-12 21:12:29 -0300

    Removed sentry

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -2,7 +2,6 @@ import asyncio
 from collections import namedtuple
 
 import exrex
-import raven
 from discord.ext import commands
 
 import config
@@ -35,7 +34,6 @@ class Context(commands.Context):
         self.token = exrex.getone(
             r'([NM][a-zA-Z\d]{23}[.][a-zA-Z\d]{6}[.][a-zA-Z\d]{27})'
         )
-        self.sentry = raven.Client(config.sentry)
         self.emojis = namedtuple(
             'Emojis', 'check xmark white_check cross_mark tick_yes') \
             ('<:check:410612082929565696>',
