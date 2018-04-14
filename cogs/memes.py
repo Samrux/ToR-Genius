d0d43ca564802158f7781a079df8b8a3383525c5
Samrux <samruxb@gmail.com>
2018-04-14 00:58:14 -0300

    renamed other to memes

--- cogs/memes.py
+++ cogs/memes.py
@@ -145,7 +145,7 @@ class LinkOrAvatar(commands.Converter):
             )
 
 
-class Other:
+class Memes:
     def __init__(self, bot):
         self.bot = bot
 
@@ -153,51 +153,6 @@ class Other:
     async def __error(ctx, err):
         if isinstance(err, commands.BadArgument):
             await ctx.send(err)
-
-    @commands.command()
-    async def forum(self, ctx, *, search):
-        """Search the Swift Discourse Forum for anything."""
-        with aiohttp.ClientSession() as s:
-            async with s.get(
-                    'https://forums.swift.org/search/query.json',
-                    params={'term': search}
-            ) as r:
-                r = await r.json()
-
-                if r['grouped_search_result'] is None:
-                    return await ctx.send('No results found.')
-
-                data = []
-
-                # I'm sorry. (Ok not as bad now)
-
-                # idk why, but topics seems to disappear sometimes
-                data.extend([(f't/{t["id"]}', t['title'])
-                             for t in r.get('topics', [])])
-
-                data.extend([(f'u/{u["username"]}',
-                              f'{u["username"]} ({u["name"]})')
-                             for u in r['users']])
-
-                data.extend([(f'c/{c.id}', c['name'])
-                             for c in r['categories']])
-
-                data.extend([(f'tags/{t["name"]}', t['name'])
-                             for t in r['tags']])
-
-                data.extend([(f'p/{p["id"]}', p['blurb'])
-                             for p in r['posts']])
-
-                if not data:
-                    return await ctx.send('No results found.')
-
-                p = Pages(
-                    ctx,
-                    entries=[f'[{d[1]}](https://forums.swift.org/{d[0]})'
-                             for d in data]
-                )
-
-                await p.paginate()
 
     # noinspection PyUnresolvedReferences,PyPep8Naming
     @commands.command()
@@ -489,4 +444,4 @@ class Other:
 
 
 def setup(bot):
-    bot.add_cog(Other(bot))
+    bot.add_cog(Memes(bot))
